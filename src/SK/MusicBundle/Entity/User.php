6199dfe02befe2b889c1d1260f9721ba07244b0a
thsking <thskingperso@gmail.com>
2018-02-16 17:33:10 +0100

    oc

---
+++ src/SK/MusicBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+// src/SK/MusicBundle/Entity/User.php
+
+namespace SK\MusicBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
